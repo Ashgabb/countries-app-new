bea1eb7aea6f712ad35b1d75f496bd131ce17c2d
Ashgabb <niiamarhgabriel@gmail.com>
2024-01-15 13:08:29 +0000

    Project debugged and changes applied

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Countries from "./Components/Countries";
+import SingleCountry from "./Components/SingleCountry";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Countries />}></Route>
+        <Route path="/:name" element={<SingleCountry />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
